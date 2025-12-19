7e4600bf58e12921560c8d3b32cd2e9110565782
Meshak Otieno <meshakotieno343@gmail.com>
2025-12-19 19:20:47 +0300

    added render link

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://blog-sum-rna1.vercel.app/',
+  // baseURL: 'https://blog-sum-rna1.vercel.app/',
+  baseURL: 'https://blogsum123.onrender.com',
   // baseURL: 'http://localhost:5000',
 });
 
